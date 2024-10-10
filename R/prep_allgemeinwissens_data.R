7b353c489671c8e2e602fc831406937929b7c2d5
Nicklas Hafiz <nicklashaf@gmail.com>
2024-10-10 14:15:48 +0200

    prepped allgemeinwissens daten neu

--- R/prep_allgemeinwissens_data.R
+++ R/prep_allgemeinwissens_data.R
@@ -31,8 +31,8 @@ library(tidyverse)
 
 # gads <- readRDS("/home/nick/Downloads/ZA6268_v1-0-0_gads.rds")
 # gads$dat <- gads$dat %>%
-#    filter(C8 == 4)
-# saveRDS(gads, "/home/nick/Downloads/student_pisa_4.rds")
+##    filter(C8 == 4)
+ #saveRDS(gads, "/home/nick/Downloads/student_pisa_4.rds")
 
 sp4 <- readRDS("/home/nick/Downloads/student_pisa_4.rds")
 
@@ -55,7 +55,12 @@ questions <- gads$labels %>%
   unique()
 
 answers_4 <- sp4$dat %>%
-  select(C8, T1, C32, all_of(answer_cols)) %>%
+  select(C8, T1, C32, all_of(answer_cols), C38, C18, N33 ) %>% # C38 Bundesland Studium, C18 Spiegel Lesefrequenz, N33 Alter
+  filter(C38 %in% c(2, 3)) %>%
+  rename("spiegelReadingfreq" = C18, 
+         "age" = N33, 
+         "bundeslandStudium" = C38) %>%
+  #select(C8, T1, C32, all_of(answer_cols)) %>%
   rename("gender" = C32) %>%
   pivot_longer(
     cols = matches("_"),
@@ -73,13 +78,13 @@ q_a_4 <- answers_4 %>%
 
 
 # Subset auswählen --------------------------------------------------------
-qa_dat <- readRDS(here::here("raw_data", "q_a.rds"))
+# qa_dat <- readRDS(here::here("raw_data", "q_a.rds"))
 
-qa_dat_3 <- qa_dat %>%
+qa_dat_3 <- q_a_4 %>%
   filter(str_starts(question_code, "3")) %>%
   select(-C8)
 
-saveRDS(qa_dat_3, here::here("raw_data", "q_a_wirt.rds"))
+saveRDS(qa_dat_3, here::here("raw_data", "q_a_wirt_bb.rds"))
 
 
 # With haven for quick check -------------------------------------
@@ -88,3 +93,4 @@ saveRDS(qa_dat_3, here::here("raw_data", "q_a_wirt.rds"))
 ## Quicker to read as rds file:
 # saveRDS(dataset, "/home/nick/Downloads/ZA6268_v1-0-0.rds")
 # dataset <- readRDS("/home/nick/Downloads/ZA6268_v1-0-0.rds")
+
